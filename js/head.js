0b2965df36007f20d044544a22abca160066709a
Thinkpad <fgp555@gmail.com>
2023-05-12 12:07:19 -0500

    add head.js

---
+++ js/head.js
@@ -0,0 +1,11 @@
+/*
+  <script src="https://frank-gp.github.io/lib/js/head.js" defer></script>
+*/
+
+document.head.innerHTML += /*html */ `<!-- Meta -->
+<link rel="icon" href="https://asset.fgp.one/file/fgp-favicon-256px.webp" sizes="256x256" />
+<meta property="og:image" content="https://asset.fgp.one/file/fgp-fb-cover.webp" />
+<meta property="og:image:width" content="600" />
+<meta property="og:image:height" content="314" />
+<!-- CSS files -->
+`;
